71198e6eb2267d98b426a60a8779a4e400699abe
QuangGiang06 <tqgiang123456789@gmail.com>
2026-09-17 16:44:04 +0700

    Initial commit KnowledgeSpace

---
+++ src/KnowledgeSpace.WedPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KnowledgeSpace.WedPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
